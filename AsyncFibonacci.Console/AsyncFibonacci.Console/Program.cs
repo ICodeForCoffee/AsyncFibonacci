ae711d2f1ff18f957c6938974620e571946c9f84
Matthew Pouliotte <poul102@gmail.com>
2024-12-26 17:40:39 -0500

    Added some display code and refactored this.

--- AsyncFibonacci.Console/AsyncFibonacci.Console/Program.cs
+++ AsyncFibonacci.Console/AsyncFibonacci.Console/Program.cs
@@ -1,21 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AsyncFibonacci.Console;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Async Performance Tester");
 
-for (int i = 0; i < 3; i++)
+ExecuteComparison(20);
+ExecuteComparison(30);
+ExecuteComparison(35);
+ExecuteComparison(40);
+
+async void ExecuteComparison(int fibonacciNumber)
 {
+    Console.WriteLine($"Calculating the {fibonacciNumber}\n");
     var watch1 = System.Diagnostics.Stopwatch.StartNew();
-    var value1 = await FibonacciAsync.GetFibonacciNumberAsync(40);
+    var value1 = await FibonacciAsync.GetFibonacciNumberAsync(fibonacciNumber);
     watch1.Stop();
     var elapsedMs1 = watch1.ElapsedMilliseconds;
 
 
     var watch2 = System.Diagnostics.Stopwatch.StartNew();
-    var value2 = FibonacciNoAsync.GetFibonacciNumber(40);
+    var value2 = FibonacciNoAsync.GetFibonacciNumber(fibonacciNumber);
     watch2.Stop();
     var elapsedMs2 = watch2.ElapsedMilliseconds;
 
-    Console.WriteLine($"Async took {elapsedMs1}");
-    Console.WriteLine($"Regular took {elapsedMs2}");
+    Console.WriteLine($"Async took {elapsedMs1} ms");
+    Console.WriteLine($"Regular took {elapsedMs2} ms\n");
 }
